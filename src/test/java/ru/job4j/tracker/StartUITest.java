dba337ee8a1d7cd020b4a90b07f12af6e7e4978a
SkMihail <msklyarov@gmail.com>
2023-01-29 17:26:09 +1000

    12.0. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,27 +9,30 @@ class StartUITest {
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(new String[]{"0", "Test", "1"});
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        UserAction[] action = {new CreateAction(), new ExitAction()};
-        new StartUI().init(in, tracker, action);
+        UserAction[] action = {new CreateAction(out), new ExitAction(out)};
+        new StartUI(out).init(in, tracker, action);
         assertThat(tracker.findById(1).getName()).isEqualTo("Test");
     }
 
     @Test
     public void myTestWhenEditItem() {
         Input in = new StubInput(new String[]{"0", "Test Name", "1", "1", "New Name", "2"});
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        UserAction[] action = {new CreateAction(), new EditAction(), new ExitAction()};
-        new StartUI().init(in, tracker, action);
+        UserAction[] action = {new CreateAction(out), new EditAction(out), new ExitAction(out)};
+        new StartUI(out).init(in, tracker, action);
         assertThat(tracker.findById(1).getName()).isEqualTo("New Name");
     }
 
     @Test
     public void myTestWhenDeleteItem() {
         Input in = new StubInput(new String[]{"0", "Test", "1", "1", "2"});
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        UserAction[] action = {new CreateAction(), new DeleteAction(), new ExitAction()};
-        new StartUI().init(in, tracker, action);
+        UserAction[] action = {new CreateAction(out), new DeleteAction(out), new ExitAction(out)};
+        new StartUI(out).init(in, tracker, action);
         assertThat(tracker.findById(1)).isNull();
     }
 
@@ -38,9 +41,10 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
-        Input in = new StubInput(new String[]{"0", "1", replacedName, "1"});
-        UserAction[] actions = {new EditAction(), new ExitAction()};
-        new StartUI().init(in, tracker, actions);
+        Input in = new StubInput(new String[]{"0", String.valueOf(item.getId()), replacedName, "1"});
+        Output out = new StubOutput();
+        UserAction[] actions = {new EditAction(out), new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
 
@@ -48,9 +52,24 @@ class StartUITest {
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
-        Input in = new StubInput(new String[]{"0", "1", "1"});
-        UserAction[] actions = {new DeleteAction(), new ExitAction()};
-        new StartUI().init(in, tracker, actions);
+        Input in = new StubInput(new String[]{"0", String.valueOf(item.getId()), "1"});
+        Output out = new StubOutput();
+        UserAction[] actions = {new DeleteAction(out), new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
+    }
+
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new ExitAction(out)};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + System.lineSeparator()
+                                + "0. Exit Program" + System.lineSeparator()
+                                + "=== Exit Program ===" + System.lineSeparator()
+        );
     }
 }
