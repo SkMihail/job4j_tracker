6259b8d199c514d587db6567e1c01def5b5b16c0
SkMihail <msklyarov@gmail.com>
2023-02-06 12:21:23 +1000

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("gmail@gmail.com", "Третий");
+        map.put("mail@mail.ru", "Первый");
+        map.put("yandex@yandex.ru", "Второй");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
