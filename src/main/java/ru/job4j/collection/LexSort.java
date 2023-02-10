ad46502ffbb23431b2b5b2adce7c93a6fa7857fc
SkMihail <msklyarov@gmail.com>
2023-02-10 13:04:32 +1000

    4. Сортировка номера [#221284] Исправление имён переменных

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,8 +6,9 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int l = Integer.parseInt(left.split(". ")[0]);
-        int r = Integer.parseInt(right.split(". ")[0]);
-        return Integer.compare(l, r);
+        int leftVar = Integer.parseInt(left.split(". ")[0]);
+        int rightVar = Integer.parseInt(right.split(". ")[0]);
+        return Integer.compare(leftVar, rightVar);
     }
 }
+
