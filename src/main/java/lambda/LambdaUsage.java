2f8485951330d5eef83cd71ade4981620e95544d
SkMihail <msklyarov@gmail.com>
2023-02-12 20:41:46 +1000

    2.1. Лямбда блок [#249208] исправление

--- src/main/java/lambda/LambdaUsage.java
+++ src/main/java/lambda/LambdaUsage.java
@@ -3,20 +3,16 @@ package lambda;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class LambdaUsage {
 
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
-        AtomicInteger count = new AtomicInteger(1);
         Comparator<String> comparator = (left, right) -> {
-            System.out.println(" [Iteration " + count + ": Compare " + left + " and "  + right + "]");
-            count.getAndIncrement();
+            System.out.println("[Compare " + left + " and "  + right + "]");
             return  Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
-
         for (String str : strings) {
             System.out.println(str);
         }
