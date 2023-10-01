eeb133df12c2604cff10415245f80e62ba3338b4
SkMihail <msklyarov@gmail.com>
2023-10-01 22:57:17 +1000

    3. Отсортировать департаменты [#34352] другой вариант.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,23 +5,18 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] o1Array = o1.split("/");
-        String[] o2Array = o2.split("/");
+        String[] o1Array = o1.split("/", 2);
+        String[] o2Array = o2.split("/", 2);
         int result = o2Array[0].compareTo(o1Array[0]);
         if (result != 0) {
             return result;
         }
-        if (Math.min(o1Array.length, o2Array.length) != 1) {
-            for (int i = 1; i < Math.min(o1Array.length, o2Array.length); i++) {
-                result = o1Array[i].compareTo(o2Array[i]);
-                if (result != 0) {
-                    return result;
-                }
-            }
+        int commonLength = o1Array.length + o2Array.length;
+        if (commonLength == 4) {
+            return o1Array[1].compareTo(o2Array[1]);
         } else {
-            result = Integer.compare(o1Array.length, o2Array.length);
+            return Integer.compare(o1Array.length, o2Array.length);
         }
-        return result;
     }
 }
 
