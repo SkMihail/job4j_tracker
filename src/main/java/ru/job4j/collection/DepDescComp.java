53e8d3279953ebc53012168358de2e9ea84bf48b
SkMihail <msklyarov@gmail.com>
2023-10-02 23:58:04 +1000

    3. Отсортировать департаменты [#34352] с тернарным оператором, самый читаемый вариант.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,15 +8,7 @@ public class DepDescComp implements Comparator<String> {
         String[] o1Array = o1.split("/", 2);
         String[] o2Array = o2.split("/", 2);
         int result = o2Array[0].compareTo(o1Array[0]);
-        if (result != 0) {
-            return result;
-        }
-        int commonLength = o1Array.length + o2Array.length;
-        if (commonLength == 4) {
-            return o1Array[1].compareTo(o2Array[1]);
-        } else {
-            return Integer.compare(o1Array.length, o2Array.length);
-        }
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
 
